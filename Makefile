54b2ce662ddc7db2d12c9e656b939a74633478a5
Camille Rouet <camille.rouet@insa-lyon.fr>
2016-01-25 16:28:55 +0100

    Ajout de la classe Mario

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 all: executable
 
-executable: test_polymorph.o Yoshi.o Character.o
-	g++ -g test_polymorph.o Yoshi.o Character.o -o executable -std=c++11
+executable: test_polymorph.o Yoshi.o Mario.o Character.o 	
+	g++ -g test_polymorph.o Yoshi.o Mario.o Character.o -o executable -std=c++11
 	
-test_polymorph.o: test_polymorph.cpp Character.h Yoshi.h
+test_polymorph.o: test_polymorph.cpp Character.h Yoshi.h Mario.h
 	g++ -g -c test_polymorph.cpp -o test_polymorph.o -std=c++11
 	
 Yoshi.o: Yoshi.cpp Yoshi.h Character.h
 	g++ -g -c Yoshi.cpp -o Yoshi.o -std=c++11
+	
+Mario.o : Mario.cpp Mario.h Character.h
+	g++ -g -c Mario.cpp -o Mario.o -std=c++11
 
 Character.o: Character.cpp Character.h
 	g++ -g -c Character.cpp -o Character.o -std=c++11
--- test_polymorph.cpp
+++ test_polymorph.cpp
@@ -7,6 +7,7 @@
 #
 #include "Character.h"
 #include "Yoshi.h"
+#include "Mario.h"
 
 // ===========================================================================
 //                            Function declarations
@@ -28,26 +29,28 @@ int main(int argc, char* argv[]) {
   // Yoshi       character6 = Character(); marche pas
   Character*  character7 = new Yoshi(); //type character* donc pointeur donc le virtual est utilisé
   // pas de cast : les méthodes virtual appelent celle de Yoshi
-	  
+  Mario* character8 = new Mario();
 	  
 	  
   
   // Yoshi*      character8 = new Character(); marche pas : vérification de type
   printf("nb_instances : %d \n",Character::nb_instances() ) ;
   printf("nb_yoshi : %d \n",Yoshi::nb_yoshi() ) ;
+  printf("nb_mario : %d \n",Mario::nb_mario() ) ;
+  
   character2.Accelerate();
   character4->Accelerate();
   //character5.Accelerate();
   //character6.Accelerate();
   character7->Accelerate();
-  //character8->Accelerate();
+  character8->Accelerate();
 
   printf("Character 2 speed : %f\n", character2.speed());
   printf("Character 4 speed : %f\n", character4->speed());
   //printf("Character 5 speed : %f\n", character5.speed());
   //printf("Character 6 speed : %f\n", character6.speed());
   printf("Character 7 speed : %f\n", character7->speed());
-  //printf("Character 8 speed : %f\n", character8->speed());
+  printf("Character 8 speed : %f\n", character8->speed());
 
   printf("\n" );
 
@@ -56,26 +59,30 @@ int main(int argc, char* argv[]) {
   //character5.Break();
   //character6.Break();
   character7->Break();
-  //character8->Break();
+  character8->Break();
 
   printf("Character 2 speed : %f\n", character2.speed());
   printf("Character 4 speed : %f\n", character4->speed());
   //printf("Character 5 speed : %f\n", character5.speed());
   //printf("Character 6 speed : %f\n", character6.speed());
   printf("Character 7 speed : %f\n", character7->speed());
-  //printf("Character 8 speed : %f\n", character8->speed());
+  printf("Character 8 speed : %f\n", character8->speed());
 	
   
   delete character4;
   delete character7;
+  delete character8;
   character2.WhatAmI();
   printf("nb_instances : %d \n",Character::nb_instances() ) ;
   printf("nb_yoshi : %d \n",Yoshi::nb_yoshi() ) ;
+  printf("nb_mario : %d \n",Mario::nb_mario() ) ;
+  
   }
   
 	
   printf("nb_instances : %d \n",Character::nb_instances() ) ;
   printf("nb_yoshi : %d \n",Yoshi::nb_yoshi() ) ;
+  printf("nb_mario : %d \n",Mario::nb_mario() ) ;
   
   return 0;
 }
